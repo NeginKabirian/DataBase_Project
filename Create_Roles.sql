5341f64b37352b0f754a53c54be2fa11d8c127c7
meisampw1384 <mpwryamehr585@gmail.com>
2025-06-11 11:08:35 +0330

    feat: Add Roles

---
+++ Create_Roles.sql
@@ -0,0 +1,6 @@
+IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'EducationAdminRole' AND type = 'R') CREATE ROLE EducationAdminRole; 
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'LibrarianRole' AND type = 'R') CREATE ROLE LibrarianRole;
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'StudentRole' AND type = 'R') CREATE ROLE StudentRole;
+GO
