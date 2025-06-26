d65831d28994c5516f185f15043a584e4d7c1b47
NeginKabirian <neginka1383@gmail.com>
2025-06-26 20:53:00 +0330

    Add BorrowBook And TR_Loans_AfterUpdate_HandleReturnAndUpdateBookCopyStatus Status

--- LibraryTest.sql
+++ LibraryTest.sql
@@ -25,9 +25,10 @@ from Library.BookCopyStatuses
 select * 
 from Library.LibraryLog
 --Excecute
-
+SELECT Library.IsBookCopyAvailable(47) As IsBookCopyAvailable; --IsBookCopyAvailable Function
 EXEC Library.BorrowBook @MemberID = 4, @CopyID = 45;
-EXEC Library.BorrowBook @MemberID = 5, @CopyID = 45;
+SELECT Library.IsBookCopyAvailable(45) As IsBookCopyAvailable; --IsBookCopyAvailable Function
+EXEC Library.BorrowBook @MemberID = 5, @CopyID = 45;  --Cant Borrow
 EXEC Library.BorrowBook @MemberID = 4, @CopyID = 48;
 --Table After
 select *
@@ -49,3 +50,26 @@ FROM
     Library.LibraryMembers;
 Select Library.GetMemberActiveLoanCount(4) As LoanCount;
 Select Library.GetMemberActiveLoanCount(5) As LoanCount;
+
+
+--Library.ReturnBook Procedure
+--Excecute
+EXEC Library.ReturnBook @CopyID = 45;
+EXEC Library.ReturnBook @CopyID = 48;
+
+-- Updates the book copy status to "Available" when a book is returned.
+--Triger Test
+--Library.TR_Loans_AfterUpdate_HandleReturnAndUpdateBookCopyStatus
+SELECT Library.IsBookCopyAvailable(45) As IsBookCopyAvailable;  --
+SELECT Library.IsBookCopyAvailable(48) As IsBookCopyAvailable; --
+--Table After
+select *
+from Library.Loans
+select* 
+from Library.BookCopies
+select *
+from Library.BookCopyStatuses
+
+select * 
+from Library.LibraryLog
+
